6ffd0ffce17078e2da826f56d64144f762766c17
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-02-16 12:25:00 +0100

    Add an article

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -22,4 +22,26 @@ class ArticlesController extends AppController
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
     }
+
+    /**
+     * Add an article
+     */
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            // L'écriture de 'user_id' en dur est temporaire et
+            // sera supprimée quand nous aurons mis en place l'authentification.
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Votre article a été sauvegardé.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Impossible d\'ajouter votre article.'));
+        }
+        $this->set('article', $article);
+    }
 }
--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,11 +3,25 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// la classe Text
+use Cake\Utility\Text;
+// la classe EventInterface
+use Cake\Event\EventInterface;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config): void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // On ne garde que le nombre de caractère correspondant à la longueur
+            // maximum définie dans notre schéma
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
